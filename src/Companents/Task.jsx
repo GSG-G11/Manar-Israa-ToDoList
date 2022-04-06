047b3018e75cc71cb87a224ffac737b0597d178b
israaHamdi <israaaburayya2012@gmail.com>
2022-04-06 23:18:55 +0200

    add update function

--- src/Companents/Task.jsx
+++ src/Companents/Task.jsx
@@ -1,24 +1,56 @@
 function Task(props) {
-    const {task : {id, title, teamMamber, priority,done},deleteTask,checkTask} = props
+  const {
+    task: { id, title, teamMamber, priority, done },
+    deleteTask,
+    checkTask,
+    ChangedisplayForUpdate,
+  } = props;
 
-    return (
-
-        <tr>
-            <td className="info-member"> 
-            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="avatar 1" style={{width: '45px', height: 'auto'}} />
-            <span> {teamMamber} </span>
-            </td>
-            <td className={done? "complete" : ' '}> {title} </td>
-            <td>
-                <button  className={priority==='high'?'high':priority==='medium'? 'medium': 'low'}>{priority}</button>
-            </td>
-            <td> 
-                <button className="btn btn-Delete action " onClick={()=>deleteTask(id)}><i className='bx bx-trash-alt'></i></button>
-                <button className="btn btn-Check action " onClick={()=>checkTask(id)}><i className='bx bx-check'></i></button>
-             </td>
-        </tr>
-
-    );
+  return (
+    <tr>
+      <td className="info-member">
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+          alt="avatar 1"
+          style={{ width: "45px", height: "auto" }}
+        />
+        <span> {teamMamber} </span>
+      </td>
+      <td className={done ? "complete" : " "}> {title} </td>
+      <td>
+        <button
+          className={
+            priority === "high"
+              ? "high"
+              : priority === "medium"
+              ? "medium"
+              : "low"
+          }
+        >
+          {priority}
+        </button>
+      </td>
+      <td>
+        <button
+          className="btn btn-Delete action "
+          onClick={() => deleteTask(id)}
+        >
+          <i className="bx bx-trash-alt"></i>
+        </button>
+        <button className="btn btn-Check action " onClick={() => checkTask(id)}>
+          <i className="bx bx-check"></i>
+        </button>
+        <button
+          className="btn btn-Check action "
+          onClick={() =>
+            ChangedisplayForUpdate(id, title, teamMamber, priority)
+          }
+        >
+          <i className="bx bx-edit"></i>
+        </button>
+      </td>
+    </tr>
+  );
 }
 
 export default Task;
